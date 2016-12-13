ad2b3a6f9ca15100937698109778bf4c6849c63e
adfriedman <adfriedman@mac.com>
2016-12-13 09:48:32 -0800

    Added fretboard2degree functionality

--- guitar_exercises.py
+++ guitar_exercises.py
@@ -1,5 +1,4 @@
 import random
-from collections import defaultdict
 from enum import Enum
 
 degree_frets_lu = {
@@ -16,6 +15,19 @@ degree_frets_lu = {
     '6':  [(0,3), (3,1)],
     'b7': [(1,3), (4,1)],
     '7':  [(2,3), (0,0)] }
+degrees = degree_frets_lu.keys()
+ 
+
+def fretboard2degree_QA(sub_degrees=degrees):
+    cur_degree = random.choice(sub_degrees)
+    cur_frets = degree_frets_lu[cur_degree]
+    question = "What degree corresponds to \n{}\n? ".format(
+            Fretboard([cur_frets[random.randint(0,len(cur_frets)-1)]],
+                line_prefix='\t'))
+    acceptable_answers = [cur_degree]
+    best_answer = cur_degree
+    return question, acceptable_answers, best_answer
+
 
 mode_degrees_lu = {
     'lydian':     ['1', '2', '3', '#4', '5', '6', '7'],
@@ -69,6 +81,11 @@ class Fretboard:
         return diag
  
 
+def gen_mode_diag(mode, line_prefix=""):
+    frets = [fret for degree in mode_degrees_lu[mode]
+                for fret in degree_frets_lu[degree]]
+    return Fretboard(frets, line_prefix=line_prefix)
+
 def fretboard2mode_QA(sub_modes=modes):
     cur_mode = random.choice(sub_modes)
     question = "What mode corresponds to \n{}\n? ".format(
@@ -80,26 +97,19 @@ def fretboard2mode_QA(sub_modes=modes):
 
 
 
-def gen_mode_diag(mode, line_prefix=""):
-    frets = [fret for degree in mode_degrees_lu[mode]
-                for fret in degree_frets_lu[degree]]
-    #frets = [].join(degree_frets_lu[degree] for degree in
-    #        mode_degrees_lu[mode])
-
-    return Fretboard(frets, line_prefix=line_prefix)
-
-
-
 
 
 
 
 class Quiz:
-    def __init__(self, quiz_name, gen_QA, quiz_params=None, flags=['q','x','f']):
+    def __init__(self, quiz_name, gen_QA, quiz_params={}, flags=['q','x','f']):
         self.name = quiz_name
         self.score = 0
         self.attempted = 0
-        self.gen_QA = lambda: gen_QA(**quiz_params)
+        if quiz_params:
+            self.gen_QA = lambda: gen_QA(**quiz_params)
+        else:
+            self.gen_QA = gen_QA
         self.flags = list(flags)
         self.results = []
 
@@ -113,7 +123,6 @@ class Quiz:
                         else -float('inf')
                 )
 
-    # Bit of functional programming, cause why not?
     def ask_question(self):
         question, acceptable_answers, best_answer = self.gen_QA()
         user_input = raw_input(question)
@@ -134,7 +143,10 @@ class Quiz:
 
 
 class QuizRunner:
+    """ QuizRunner Console Application
+    """
     class Status(Enum):
+        """ Status for application state machine """
         welcome = 0
         quiz = 1
         menu = 2
@@ -142,38 +154,53 @@ class QuizRunner:
         save = 4
 
     def __init__(self, quizzes):
+        """ Initialize QuizRunner
+        Parameters:
+            quizzes - list of Quizzes
+        """
         self.quizzes = quizzes
         self.current_quiz = None
         self.status = QuizRunner.Status.welcome
 
     def do_next(self):
+        """ Performs the current action of the state machine and moves
+        to the next state.
+
+        Returns:
+            should_continue - bool
+        """
+        # Welcome Screen
         if self.status == QuizRunner.Status.welcome:
             print "Guitar Theory Exercises by adfriedman"
             self.status = QuizRunner.Status.menu
-            return 1
-
+            return True
+        # In the middle of a Quiz
         elif self.status == QuizRunner.Status.quiz:
             result, flag = self.current_quiz.ask_question()
+            # Check we aren't trying to do things other than answer
+            # question
             if flag == 'q':
-                return 0
+                return False
             elif flag == 'x':
                 self.status = QuizRunner.Status.menu
-                return 1
+                return True
             elif flag == 'f':
                 print self.current_quiz, "\n"
             else:
+                # Print whether the given answer was correct
                 print "{} ({})\n".format('yes' if result['success']
                         else 'no' , result['best_answer'])
-            return 1
-
+            return True
+        # Menu Screen
         elif self.status == QuizRunner.Status.menu:
             print "\nExercizes"
             for i,quiz in enumerate(self.quizzes):
                 print "{}) {}".format(i+1, quiz.name)
             user_input = raw_input("Choose a program or type q to quit\n")
             if user_input == 'q':
-                return 0
+                return False
             
+            # Try starting chosen program
             try: 
                 if 0 < int(user_input) <= len(self.quizzes):
                     self.current_quiz = self.quizzes[int(user_input)-1]
@@ -184,7 +211,7 @@ class QuizRunner:
             except ValueError:
                 print "Input not understood, please try again."
 
-            return 1
+            return True
             
 
     
@@ -192,6 +219,9 @@ class QuizRunner:
 
 
 if __name__ == "__main__":
+
+    # Choose a subset of the degrees
+    sub_degrees = mode_degrees_lu['lydian']
 
     # Choose a subset of the modes
     sub_modes = modes[0:2]
@@ -204,11 +234,18 @@ if __name__ == "__main__":
 
     fretboard2mode_quiz = Quiz("Fretboard2Mode",
             fretboard2mode_QA, 
-            {'sub_modes': sub_modes},
-            flags=['q','x','f'])
-    quiz_runner = QuizRunner([degrees2mode_quiz, fretboard2mode_quiz])
+            {'sub_modes': sub_modes})
 
+    fretboard2degree_quiz = Quiz("Fretboard2Degree", 
+            fretboard2degree_QA,
+            {'sub_degrees': sub_degrees})
+
+    # Construct a QuizRunner with the defined quiz instances
+    quiz_runner = QuizRunner([degrees2mode_quiz, fretboard2mode_quiz, fretboard2degree_quiz])
+
+    # Run the QuizRunner
     while quiz_runner.do_next():
+        # We reach here everytime the QuizRunner takes a step
         pass
     else:
         print "Good Bye"
